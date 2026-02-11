00751574e4e9346b00d3c16e73d210d10fa49b62
jackmontinaro <66272229+jackmontinaro@users.noreply.github.com>
2026-02-11 17:09:22 -0500

    runnign

---
+++ .svelte-kit/non-ambient.d.ts
@@ -0,0 +1,43 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/drinks" | "/drinks/[drinkId]";
+		RouteParams(): {
+			"/drinks/[drinkId]": { drinkId: string }
+		};
+		LayoutParams(): {
+			"/": { drinkId?: string };
+			"/drinks": { drinkId?: string };
+			"/drinks/[drinkId]": { drinkId: string }
+		};
+		Pathname(): "/" | "/drinks" | "/drinks/" | `/drinks/${string}` & {} | `/drinks/${string}/` & {};
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): "/robots.txt" | string & {};
+	}
+}
